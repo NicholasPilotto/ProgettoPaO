481b595878168b04c5320c62463f89214b5efb1c
Nicholas Pilotto <nicholaspilotto@gmail.com>
2020-12-17 16:08:23 +0100

    Refactoring del codice e aggiunta paramentro al costruttore di default

--- Utilities/DeepPtr/deep_ptr.hpp
+++ Utilities/DeepPtr/deep_ptr.hpp
@@ -1,23 +1,24 @@
 #ifndef DEEP_PTR_H_
 #define DEEP_PTR_H_
 template <class T>
-class deep_ptr{
-private:
-    T* ptr;
-public:
-    deep_ptr();
-    ~deep_ptr();
-    deep_ptr(const deep_ptr&);
-    deep_ptr& operator=(const deep_ptr&);
-    bool operator==(const deep_ptr&) const;
-    bool operator!=(const deep_ptr&) const;
-    bool operator<(const deep_ptr&) const;
-    bool operator<=(const deep_ptr&) const;
-    bool operator>(const deep_ptr&) const;
-    bool operator>=(const deep_ptr&) const;
-    explicit operator bool() const;
-    T& operator*() const;
-    T* operator->() const;
-    T* operator[]() const;
+class deep_ptr {
+ private:
+  T* ptr;
+
+ public:
+  deep_ptr(const T&);
+  ~deep_ptr();
+  deep_ptr(const deep_ptr&);
+  deep_ptr& operator=(const deep_ptr&);
+  bool operator==(const deep_ptr&) const;
+  bool operator!=(const deep_ptr&) const;
+  bool operator<(const deep_ptr&) const;
+  bool operator<=(const deep_ptr&) const;
+  bool operator>(const deep_ptr&) const;
+  bool operator>=(const deep_ptr&) const;
+  explicit operator bool() const;
+  T& operator*() const;
+  T* operator->() const;
+  T* operator[]() const;
 };
 #endif
