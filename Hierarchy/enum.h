9ad8e2852235321701810e0e1b7ca9d480bca353
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-02-02 23:39:15 +0100

    Risolto conflitto con classe cream

--- Hierarchy/enum.h
+++ Hierarchy/enum.h
@@ -8,24 +8,24 @@ enum bottle_size {
 };
 
 enum taste {
-  lemon = 10,
-  halzenhut = 11,
-  coffee = 12,
-  licorice = 13,
-  chocolate = 14,
-  egg = 15,
-  rum = 16,
-  cream = 17,
-  strawberry = 18,
-  berries = 19,
-  blueberry = 20,
-  ribes = 21,
-  plum = 22,
-  honey = 23,
-  dry = 24,
-  smooth = 25,
-  amable = 26,
-  mint = 27
+  _lemon = 10,
+  _halzenhut = 11,
+  _coffee = 12,
+  _licorice = 13,
+  _chocolate = 14,
+  _egg = 15,
+  _rum = 16,
+  _cream = 17,
+  _strawberry = 18,
+  _berries = 19,
+  _blueberry = 20,
+  _ribes = 21,
+  _plum = 22,
+  _honey = 23,
+  _dry = 24,
+  _smooth = 25,
+  _amable = 26,
+  _mint = 27
 };  // differenza tra smooth e amable:
     // smooth -> invecchiata
     // amable -> più amabile
