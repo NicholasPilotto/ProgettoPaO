9bbbcea62382f82e51ace4dc94b0d5f22af2b9b6
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-02-04 14:41:44 +0100

    Metodi increase_item e decrease_item

--- Receipt/receipt.cpp
+++ Receipt/receipt.cpp
@@ -13,10 +13,41 @@ void receipt::remove_item(product* p) {
 
   bool stop = false;
   for (; !stop && it != end; it++) {
-    if (typeid(*it) == typeid(*p)) {
+    if (typeid(*((*it).first)) == typeid(*p)) {
       items.erase(it);
       stop = true;
     }
   }
 }
 
+void receipt::increase_item(product* p) {
+  u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
+  u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
+
+  bool stop = false;
+  for (; !stop && it != end; it++) {
+    if (typeid(*((*it).first)) == typeid(*p)) {
+      if (typeid(*((*it).first)) == typeid(*p)) {
+        ((*it).second) += 1;
+        stop = true;
+      }
+    }
+  }
+
+  if (!stop) {
+    add_item(p);
+  }
+}
+
+void receipt::decrease_item(product* p) {
+  u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
+  u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
+
+  bool stop = false;
+  for (; !stop && it != end; it++) {
+    if (((*it).second) != 0) {
+      ((*it).second) -= 1;
+      stop = true;
+    }
+  }
+}
