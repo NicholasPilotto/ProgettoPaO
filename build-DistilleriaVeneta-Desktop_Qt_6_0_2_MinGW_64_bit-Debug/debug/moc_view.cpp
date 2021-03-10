2e6a9313867c2e735ed03d99f68c09f7c851b098
filippobrugnolaro <filippo.brugnolaro.fb@gmail.com>
2021-03-10 14:36:27 +0100

    Compilazione nuovo pc

---
+++ build-DistilleriaVeneta-Desktop_Qt_6_0_2_MinGW_64_bit-Debug/debug/moc_view.cpp
@@ -0,0 +1,161 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'view.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.0.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../DistilleriaVeneta/view.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'view.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.0.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_view_t {
+    const uint offsetsAndSize[2];
+    char stringdata0[5];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_view_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_view_t qt_meta_stringdata_view = {
+    {
+QT_MOC_LITERAL(0, 4) // "view"
+
+    },
+    "view"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_view[] = {
+
+ // content:
+       9,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void view::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    (void)_o;
+    (void)_id;
+    (void)_c;
+    (void)_a;
+}
+
+const QMetaObject view::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_view.offsetsAndSize,
+    qt_meta_data_view,
+    qt_static_metacall,
+    nullptr,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *view::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *view::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_view.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int view::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    return _id;
+}
+struct qt_meta_stringdata_QProduct_t {
+    const uint offsetsAndSize[2];
+    char stringdata0[9];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_QProduct_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_QProduct_t qt_meta_stringdata_QProduct = {
+    {
+QT_MOC_LITERAL(0, 8) // "QProduct"
+
+    },
+    "QProduct"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_QProduct[] = {
+
+ // content:
+       9,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void QProduct::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    (void)_o;
+    (void)_id;
+    (void)_c;
+    (void)_a;
+}
+
+const QMetaObject QProduct::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_QProduct.offsetsAndSize,
+    qt_meta_data_QProduct,
+    qt_static_metacall,
+    nullptr,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *QProduct::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *QProduct::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_QProduct.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int QProduct::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
