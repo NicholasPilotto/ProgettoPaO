448d790f3b7744e5b1f39a7c19a533d78ba6502f
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-03-30 21:12:41 +0200

    Importazione file receipt

---
+++ DistilleriaVeneta/receipt.cpp
@@ -0,0 +1,69 @@
+#include "receipt.h"
+
+receipt::receipt() : date(steady_clock::now()), items(u_vector<pair<deep_ptr<product>, int>>()) {}
+
+void receipt::add_item(product* p) {
+  pair<deep_ptr<product>, int> element(p, 1);
+  items.push_back(element);
+}
+
+void receipt::remove_item(product* p) {
+  u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
+  u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
+
+  bool stop = false;
+  for (; !stop && it != end; it++) {
+    if (typeid(*((*it).first)) == typeid(*p)) {
+      items.erase(it);
+      stop = true;
+    }
+  }
+}
+
+void receipt::increase_item(product* p) {
+  u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
+  u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
+
+  bool stop = false;
+  for (; !stop && it != end; it++) {
+    if (typeid(*((*it).first)) == typeid(*p)) {
+      if (typeid(*((*it).first)) == typeid(*p)) {
+        ((*it).second) += 1;
+        stop = true;
+      }
+    }
+  }
+
+  if (!stop) {
+    add_item(p);
+  }
+}
+
+void receipt::decrease_item(product* p) {
+  u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
+  u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
+
+  bool stop = false;
+  for (; !stop && it != end; it++) {
+    if (((*it).second) != 0) {
+      ((*it).second) -= 1;
+      stop = true;
+    }
+  }
+}
+
+double receipt::total_price() const {
+  double price = 0;
+  u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
+  u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
+
+  for (; it != end; it++) {
+    price += (it->first->get_price() * it->second);
+  }
+
+  return price;
+}
+
+u_vector<pair<deep_ptr<product>, int>> receipt::get_items() const {
+  return items;
+}
---
+++ DistilleriaVeneta/receipt.h
@@ -0,0 +1,32 @@
+#ifndef __RECEIPT_H__
+#define __RECEIPT_H__
+
+#include <chrono>
+
+#include "Hierarchy/product.h"
+#include "Utilities/deep_ptr.hpp"
+#include "Utilities/u_vector.hpp"
+
+using std::pair;
+using std::chrono::steady_clock;
+using std::chrono::time_point;
+
+class receipt {
+ private:
+  time_point<steady_clock> date;
+  u_vector<pair<deep_ptr<product>, int>> items;
+  // u_vector<pair<int, int>> package;
+
+ public:
+  receipt();
+
+  void add_item(product*);
+  void remove_item(product*);
+  void increase_item(product*);
+  void decrease_item(product*);
+  double total_price() const;
+  u_vector<pair<deep_ptr<product>, int>> get_items() const;
+
+  // METODI PACKAGE
+};
+#endif  // __RECEIPT_H__
