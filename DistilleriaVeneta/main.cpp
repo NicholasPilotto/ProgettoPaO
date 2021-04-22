91438d10b01497e0347ae6b26be51c340f9c8cb5
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-04-22 16:44:13 +0200

    Tolto codice di test

--- DistilleriaVeneta/main.cpp
+++ DistilleriaVeneta/main.cpp
@@ -2,14 +2,10 @@
 
 #include "./Utilities/deep_ptr.hpp"
 #include "./Utilities/u_vector.hpp"
-#include "io_json.h"
 #include "view.h"
 
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
-
-	io_json* io = new io_json(":/data/data.json");
-	u_vector<deep_ptr<product>> products = io->read();
 	view v;
 	v.show();
 	return a.exec();
