900370663e267369138d4158b7393e75e789f6dd
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-05-02 16:12:11 +0200

    Modifiche in base ai costruttori delle 3 classi

--- DistilleriaVeneta-Backup/main.cpp
+++ DistilleriaVeneta-Backup/main.cpp
@@ -1,12 +1,20 @@
 #include <QApplication>
+#include <QDebug>
 
 #include "./Utilities/deep_ptr.hpp"
 #include "./Utilities/u_vector.hpp"
+#include "controller.h"
+#include "model.h"
 #include "view.h"
 
-int main(int argc, char *argv[]) {
-	QApplication a(argc, argv);
-	view v;
-	v.show();
-	return a.exec();
+int main(int argc, char* argv[]) {
+  QApplication a(argc, argv);
+  model m;
+  controller c(&m);
+
+  view w(&c);
+  c.link_view(&w);
+
+  w.show();
+  return a.exec();
 }
