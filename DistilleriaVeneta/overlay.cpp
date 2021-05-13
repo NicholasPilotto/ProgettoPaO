672b20cca797b79e9d4e43c15ce58b73b7468364
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-05-13 10:28:12 +0200

    Classe necessaria per l'aggiunta dell'overlay nella griglia dei prodotti

---
+++ DistilleriaVeneta/overlay.cpp
@@ -0,0 +1,24 @@
+#include "overlay.h"
+
+QOverlay::QOverlay(QWidget* parent) : QWidget(parent) {
+
+  // Creazione layout
+
+  overlay_layout = new QVBoxLayout(this);
+
+  // Creazione bottoni
+
+  small_button = new QPushButton("Small",this);
+  medium_button = new QPushButton("Medium",this);
+  large_button = new QPushButton("Large",this);
+
+  // Aggiunta bottoni
+
+  overlay_layout->addWidget(small_button);
+  overlay_layout->addWidget(medium_button);
+  overlay_layout->addWidget(large_button);
+
+  // Set Layout
+
+  setLayout(overlay_layout);
+}
