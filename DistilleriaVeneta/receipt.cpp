104b61f979fc9f6ad3a4ea470e3b4a30fc10e796
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-05-13 10:24:15 +0200

    Nuova versione receipt

--- DistilleriaVeneta/receipt.cpp
+++ DistilleriaVeneta/receipt.cpp
@@ -1,80 +1,85 @@
 #include "receipt.h"
 
-receipt::receipt() : date(steady_clock::now()), items(u_vector<pair<deep_ptr<product>, int>>()) {}
+receipt::receipt() : date(steady_clock::now()), items({}) {}
 
-void receipt::add_item(product* p) {
+void receipt::add_item(const deep_ptr<product>& p) {
   pair<deep_ptr<product>, int> element(p, 1);
   items.push_back(element);
 }
 
-void receipt::remove_item(product* p) {
+void receipt::remove_item(const deep_ptr<product>& p) {
   u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
   u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
 
   bool stop = false;
+
   for (; !stop && it != end; it++) {
-    if (typeid(*((*it).first)) == typeid(*p)) {
+    if ((*it).first->get_name() == p->get_name() && (*it).first->get_kind() == p->get_kind()) {
       items.erase(it);
       stop = true;
     }
   }
 }
 
-void receipt::increase_item(product* p) {
-  u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
-  u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
+void receipt::delete_all() {
+    u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
+    u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
 
-  bool stop = false;
-  for (; !stop && it != end; it++) {
-    if (typeid(*((*it).first)) == typeid(*p)) {
-      if (typeid(*((*it).first)) == typeid(*p)) {
-        ((*it).second) += 1;
-        stop = true;
-      }
+    for(; it != end; it++) {
+        items.erase(it);
     }
-  }
-
-  if (!stop) {
-    add_item(p);
-  }
 }
 
-void receipt::decrease_item(product* p) {
-  u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
-  u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
+bool receipt::presenza(const deep_ptr<product>& p) const{
+    u_vector<pair<deep_ptr<product>, int>>::const_iterator cit = items.const_begin();
+    u_vector<pair<deep_ptr<product>, int>>::const_iterator end = items.const_end();
 
-  bool stop = false;
-  for (; !stop && it != end; it++) {
-    if (((*it).second) != 0) {
-      ((*it).second) -= 1;
-      stop = true;
+    for(; cit != end; cit++) {
+         if ((*cit).first->get_name() == p->get_name() && (*cit).first->get_kind() == p->get_kind())
+             return true;
     }
+    return false;
+}
+
+unsigned int receipt::total_number_items() const{
+  unsigned int count = 0;
+  u_vector<pair<deep_ptr<product>, int>> aux = get_items();
+  auto cit = aux.const_begin();
+  auto end = aux.const_end();
+
+  for (; cit != end; cit++) {
+    count += (*cit).second;
   }
+  return count;
 }
 
 double receipt::total_price() const {
   double price = 0;
+  u_vector<pair<deep_ptr<product>, int>>::const_iterator cit = items.const_begin();
+  u_vector<pair<deep_ptr<product>, int>>::const_iterator end = items.const_end();
+
+  for (int i=0; cit != end; cit++, i++) {
+    price += total_price_line(i);
+  }
+  return price;
+}
+
+double receipt::total_price_line(unsigned int i) const {
+    double line;
+  pair<deep_ptr<product>, int> pos = items.at(i);
+  line = pos.first->get_price() * pos.second;
+  return line;
+}
+
+double receipt::total_taxes() const {
+  double tot_taxes = 0;
   u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
   u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
 
   for (; it != end; it++) {
-    price += (it->first->get_price() * it->second);
+    tot_taxes += (it->first->taxes() * it->second);
   }
-
-  return price;
+  return tot_taxes;
 }
 
 u_vector<pair<deep_ptr<product>, int>> receipt::get_items() const { return items; }
-
-double receipt::total_taxes() const
-{
-      double taxes = 0;
-      u_vector<pair<deep_ptr<product>, int>>::iterator it = items.begin();
-      u_vector<pair<deep_ptr<product>, int>>::iterator end = items.end();
-
-      for (; it != end; it++) {
-        taxes += (it->first->taxes() * it->second);
-      }
-
-      return taxes;
-}
--- DistilleriaVeneta/receipt.h
+++ DistilleriaVeneta/receipt.h
@@ -15,18 +15,17 @@ class receipt {
  private:
   time_point<steady_clock> date;
   u_vector<pair<deep_ptr<product>, int>> items;
-  // u_vector<pair<int, int>> package;
 
  public:
   receipt();
-
-  void add_item(product*);
-  void remove_item(product*);
-  void increase_item(product*);
-  void decrease_item(product*);
+  void add_item(const deep_ptr<product>&);
+  void remove_item(const deep_ptr<product>&);
+  void delete_all();
+  bool presenza(const deep_ptr<product>&) const;
+  unsigned int total_number_items() const ;
+  double total_price_line(unsigned int i) const;
   double total_price() const;
-  u_vector<pair<deep_ptr<product>, int>> get_items() const;
   double total_taxes() const;
-  // METODI PACKAGE
+  u_vector<pair<deep_ptr<product>, int>> get_items() const;
 };
 #endif  // __RECEIPT_H__
