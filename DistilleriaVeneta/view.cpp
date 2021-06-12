0099063abc7b1b01abaac53e63d140c59e67f8e9
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-06-12 13:16:12 +0200

    Miglioria filtri

--- DistilleriaVeneta/view.cpp
+++ DistilleriaVeneta/view.cpp
@@ -87,11 +87,11 @@ QHBoxLayout* view::add_filter_buttons() {
   grappa_button = new QPushButton("Grappa", this);
   grappa_button->setObjectName("old,young");
   liquor_button = new QPushButton("Liquore", this);
-  liquor_button->setObjectName("liquor");
+  liquor_button->setObjectName("liquor,null");
   cream_button = new QPushButton("Crema", this);
-  cream_button->setObjectName("cream");
+  cream_button->setObjectName("cream,null");
   all_button = new QPushButton("Tutti", this);
-  all_button->setObjectName("all");
+  all_button->setObjectName("all,null");
 
   // Aggiunta bottoni
 
